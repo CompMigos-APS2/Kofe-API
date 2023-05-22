0e2f83008953f082b2b2a44e1b4cf0b07315a83f
pamella-silva <90103227+pampamella@users.noreply.github.com>
2023-05-22 11:38:14 -0300

    conexão teste com postgres

--- initial/src/main/java/com/application/Application.java
+++ initial/src/main/java/com/application/Application.java
@@ -7,7 +7,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
+@EntityScan(basePackages = {
+        "com.application.entities"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.application.repository"
+})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
