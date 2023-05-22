0df3f742fbdbb6f52c21c9c664badc89cae5b5f7
Vítor Nascimento <vitorepn11@gmail.com>
2023-05-22 08:48:06 -0300

    conexão teste com postgres

--- initial/src/main/java/com/application/Application.java
+++ initial/src/main/java/com/application/Application.java
@@ -10,10 +10,21 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableAsync
+@EntityScan(basePackages = {
+        "com.application.entities"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.application.repository"
+})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
