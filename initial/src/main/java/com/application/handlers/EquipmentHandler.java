bd961003fb115134b1ac06846718f3faeea4ea9f
pamella-silva <90103227+pampamella@users.noreply.github.com>
2023-05-26 17:33:01 -0300

    impl equipmentRepository e equipmentHandler

---
+++ initial/src/main/java/com/application/handlers/EquipmentHandler.java
@@ -0,0 +1,40 @@
+package com.application.handlers;
+
+import com.application.entities.Equipment;
+import com.application.entities.EquipmentType;
+import com.application.repository.EquipmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/equipment")
+public class EquipmentHandler extends GenericHandler<Equipment, EquipmentRepository> {
+    @Autowired
+    public EquipmentHandler(EquipmentRepository repository) {
+        super(repository);
+    }
+    @RequestMapping("/getByType")
+    public ResponseEntity<List<Equipment>> getByType(EquipmentType type){
+        return new ResponseEntity<>(repository.findByType(type), HttpStatus.OK);
+    }
+
+
+//    @RequestMapping("/deleteEquipmentById")
+//    public ResponseEntity<List<Object>> deleteEquipmentById(String id) {
+//
+//        UUID formattedId = UUID.fromString(id);
+//        List<Object> recipeList = repository.findRecipesWithEquipment(formattedId);
+//        if(recipeList.size() == 0) {
+//            repository.deleteById(formattedId);
+//            return new ResponseEntity<>(recipeList, HttpStatus.NO_CONTENT);
+//        }
+//        return new ResponseEntity<>(recipeList, HttpStatus.CONFLICT);
+//    }
+
+}
