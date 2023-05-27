ea32ec5ac74229e4b12d6342c69d78ac5f10d626
pamella-silva <90103227+pampamella@users.noreply.github.com>
2023-05-27 19:03:43 -0300

    remove unused files

--- initial/src/main/java/com/application/states/RecipeState.java
+++
@@ -1,9 +0,0 @@
-//package com.application.states;
-//
-//import com.application.base.BaseState;
-//import com.application.entities.Recipe;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class RecipeState extends BaseState<Recipe> {
-//}
