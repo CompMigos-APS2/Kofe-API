1e954edecdc32a17ab896850db0c879cbadf587d
matheus0andrade <matheubarreto@hotmail.com>
2023-06-07 22:23:46 -0300

    add @Validated in RecipeHandler save

--- initial/src/main/java/com/application/handlers/RecipeHandler.java
+++ initial/src/main/java/com/application/handlers/RecipeHandler.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.UUID;
 
@@ -36,6 +37,7 @@ public class RecipeHandler extends GenericHandler<Recipe, RecipeRepository> {
         super(repository);
         this.filter = new RecipeFilter(em);
     }
+    @Validated
     @PostMapping("/save")
     public ResponseEntity<Recipe> save(@RequestBody Recipe obj) {
         UUID userId = obj.getUserId();
